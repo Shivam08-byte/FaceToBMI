e7c81297dcef66043619e1c86a31600663a6242f
trungpq <trungpq.ml@gmail.com>
2020-05-12 15:07:44 +0800

    Update statistics summary for testing

--- src/config.py
+++ src/config.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import sys
 
+
 class Config:
     # Set up Variable
     data_path = "../data/"
@@ -17,10 +18,11 @@ class Config:
     processed_path = "../data/processed/"
 
     base_learning_rate = 1e-5
-    epochs = 1 #should be 500 change to 400000 if you are a rich kid!
+    epochs = 1  # should be 500 change to 400000 if you are a rich kid!
     weight_decay = 0.0005
     gamma = 0.001
     power = 0.75
-    num_of_tries = 5
+    num_of_tries = 1
+
 
 cfg = Config()
